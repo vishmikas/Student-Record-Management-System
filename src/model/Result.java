87eb690ff519bfbf3b30c01dc480f99d8c3029ef
Dilum Samarathunga <75325717+vishmikas@users.noreply.github.com>
2026-01-31 18:39:27 +0530

    Created Result class

---
+++ src/model/Result.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Result {
+    private int resultId;
+    private int studentId;
+    private String subject;
+    private int marks;
+
+    public Result() {}
+
+    public Result(int resultId, int studentId, String subject, int marks) {
+        this.studentId = studentId;
+        this.subject = subject;
+        this.marks = marks;
+    }
+
+    public int getResultId() {
+        return resultId;
+    }
+    public void setResultId(int resultId) {
+        this.resultId = resultId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
